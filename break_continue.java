e1e5a2cf46945cccb9d44ff0062eabd37eab66ae
code-bajju <11212766@mmec.mmdu.org>
2023-02-07 11:23:37 +0530

    Break and continue

---
+++ break_continue.java
@@ -0,0 +1,12 @@
+class break_continue {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            System.out.println(i);
+         if (i==3) {
+       
+            System.out.println("here is break");
+            continue;
+         }
+        }
+    }
+}
