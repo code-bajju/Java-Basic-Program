8b4e9774ba865fd419684581295385a800251bde
code-bajju <11212766@mmec.mmdu.org>
2023-02-08 11:27:04 +0530

    practice set

---
+++ cwh_ps.java
@@ -0,0 +1,50 @@
+public class cwh_ps {
+    public static void main(String[] args) {
+
+        // qn 1
+
+        // int a = 28;
+        // int b = 39;
+        // int c = 93;
+        // int sum = a+b+c;
+        // System.out.println(sum);
+
+        // qn 2
+
+        // double sub1 = 80;
+        // double sub2 = 80;
+        // double sub3 = 80;
+        // double ttl_marks = sub1+sub2+sub3 ;
+        // System.out.println(((ttl_marks)/3)/10 );
+
+        // qn3
+
+        // Scanner nm = new Scanner(System.in);
+        // System.out.println("Enter the name to curse someone");
+        // String name = nm.next();
+        // System.out.println("hello " +name+ " have a bad day bro.");
+        // nm.close();
+
+        // qn4
+
+        // Scanner km = new Scanner(System.in);
+        // System.out.println("Enter the kiloemeter to convert");
+        // double kilometers = km.nextDouble();
+        // double miles = 1.60934*kilometers;
+        // System.out.println(kilometers+" km is "+miles+" in miles.");
+        // km.close();
+
+        // qn5
+
+        // Scanner nm = new Scanner(System.in);
+        // System.out.println("Enter the number to check whether number is int or not");
+        // if (nm.hasNextInt()){
+        // System.out.println("tru");
+        // }
+        // else{
+        // System.out.println("fac");
+        // }
+        // nm.close();
+
+    }
+}
