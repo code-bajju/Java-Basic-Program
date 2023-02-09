03778a2819f66ae1842dba63ee57b2a35075fbe5
code-bajju <11212766@mmec.mmdu.org>
2023-02-09 11:27:32 +0530

    practice set-2

---
+++ cws_2.java
@@ -0,0 +1,14 @@
+public class cws_2{
+    public static void main(String[] args) {
+       byte a = 5;
+       int b = 6;
+       short c = 7;
+       int d = b+c;
+       float e = 6.54f + a;
+
+
+System.out.println(b++);
+System.out.println(b);
+
+    }
+}
