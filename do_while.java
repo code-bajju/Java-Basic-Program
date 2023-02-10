0dd78245d406501eb116fd2c7bd975ab3c2a1ed3
code-bajju <11212766@mmec.mmdu.org>
2023-02-10 11:28:24 +0530

    Conditional-do while

---
+++ do_while.java
@@ -0,0 +1,11 @@
+public class do_while {
+    public static void main(String[] args) {
+        int a = 200;
+        do {
+            System.out.println(a);
+            a++;
+        } while (a < 30);
+
+    }
+
+}
