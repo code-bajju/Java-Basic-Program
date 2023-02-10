578780c880d1f8bde3903b9e8d87babe2e4323ba
code-bajju <11212766@mmec.mmdu.org>
2023-02-10 11:28:49 +0530

    Conditional-ifelse

---
+++ if_else.java
@@ -0,0 +1,12 @@
+public class if_else {
+    public static void main(String[] args) {
+        float f = 5f;
+        boolean cond = (f==6f);
+        if (cond) {
+            System.out.println("Yes Pappu You can drive");
+        }
+        else{
+            System.out.println("Bhakk sale");
+        }
+    }    
+}
