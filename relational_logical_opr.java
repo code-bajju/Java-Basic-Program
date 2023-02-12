5f65a8b1a8148008f97340c0d4188031092581d0
code-bajju <11212766@mmec.mmdu.org>
2023-02-12 11:31:49 +0530

    logical-opr

---
+++ relational_logical_opr.java
@@ -0,0 +1,29 @@
+import javax.print.FlavorException;
+
+public class relational_logical_opr {
+  public static void main(String[] args) {
+    System.out.println("For Logical AND...");
+    boolean a = true;
+    boolean b = true;
+
+    if (a == true && b == true) {
+      System.out.println("Yes");
+
+    } else {
+      System.out.println("no");
+    }
+  }
+  {
+    System.out.println("For Logical OR...");
+    boolean a1 = true;
+    boolean b1 = true;
+
+    if (a1 == true || b1 == true) {
+      System.out.println("Yes");
+
+    } else {
+      System.out.println("no");
+    }
+  }
+
+}
