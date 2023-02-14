9d5604176c75e6554e2527b6b3eff89d96301f26
code-bajju <11212766@mmec.mmdu.org>
2023-02-14 11:34:27 +0530

    String Methods

---
+++ string_method_ps.java
@@ -0,0 +1,31 @@
+
+public class string_method_ps{
+    public static void main(String[] args) {
+        // //problem 1
+        // String name = "Bajrang Gour";
+        //         System.out.println(name.trim());
+
+        // //problem 2
+        String nidhi = "My name is Bajrang";
+       // text = text.replace("", "_");
+        System.out.println(nidhi.replace("Bajrang", "Nidhi"));
+
+        // //problem 3
+        // String letter="dear <|name|>,Thanks alot!";
+        // letter=letter.replace("<|name|>", "bajrang");
+        // System.out.println(letter);
+
+        // //problem 4
+        // String myString ="This string contain  and     space.";
+        // System.out.println(myString.indexOf("  "));
+        // System.out.println(myString.indexOf("   "));
+
+        // //problem 5
+        // String letter2="Dear bajrang Gour \n\tThis fuckin course is wrost !\n\t You lier !";
+        // System.out.println(letter2);
+
+
+    }
+
+
+}
