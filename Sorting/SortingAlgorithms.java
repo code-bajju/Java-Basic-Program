08613803b96b6c2a15e44dba08de681e26ac9e4e
code-bajju <11212766@mmec.mmdu.org>
2023-03-29 22:08:57 +0530

    Heap Sort

--- Sorting/SortingAlgorithms.java
+++ Sorting/SortingAlgorithms.java
@@ -92,4 +92,113 @@ public class SortingAlgorithms {
         while (i < n1) {
             arr[k] = L[i];
             i++;
+            k++;
+        }
+    
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    
+    // Quick Sort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot) {
+                i++;
+    
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+    
+        return i + 1;
+    }
+    
+    // Heap Sort
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+    
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+    
+        // One by one extract an element from heap
+        for (int i = n - 1; i >= 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+    
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+    
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+    
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+    
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+    
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+    
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+    
+    // Driver code to test the above sorting methods
+    public static void main(String[] args) {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+    
+        System.out.println("Original array:");
+        printArray(arr);
+    
+        // Bubble Sort
+        bubbleSort(arr);
+        System.out.println("Array after Bubble Sort:");
+        printArray(arr);
+    
+        // Selection Sort
+        selectionSort(arr);
+        System.out.println("Array after Selection Sort:");
+        printArray(arr);
+    
+        // Insertion Sort
+        insertionSort(arr);
+        System.out.println("Array after
+    
            
